54de708c3e03af14deefd001ceeffc5f8a4bd6d5
J. David Lowe <dlowe@woot.local>
2011-06-05 21:02:44 -0700

    fix sears.com

--- poot-poot/pootify.py
+++ poot-poot/pootify.py
@@ -4,6 +4,8 @@
 ## standard
 import re
 import logging
+import urlparse
+import urllib
 
 ## app engine
 from google.appengine.ext import webapp
@@ -19,39 +21,54 @@ class Pootifier(webapp.RequestHandler):
     def get(self):
         """pootify a web page"""
 
+        ## no url: back to the start!
         url = self.request.get('url')
         if not url:
             self.redirect("/pootify.html")
             return
 
-        result    = urlfetch.fetch(url, follow_redirects=True)
-        content   = result.content
+        ## parse url, or back to the start!
+        try:
+            parts = urlparse.urlparse(url)
+            if not parts.scheme:
+                raise Exception("argh!")
+        except:
+            self.redirect("/pootify.html")
+            return
 
-        ## if we got redirected, switch to the final url
-        if result.final_url:
-            url = result.final_url
+        ## fetch content or explode
+        try:
+            result = urlfetch.fetch(url, follow_redirects=False)
+        except:
+            self.response.set_status(500)
+            self.response.out.write("argh!")
+            return
 
-        parts = str(url).split('/')
+        ## make redirection the client's job
+        if result.status_code in (301, 302):
+            self.redirect(integration.INTEGRATIONS['APP_ROOT_URL' ] + 'pootify?url='
+                + urllib.quote(result.headers['location']))
+            return
 
-        ## if the url had exactly 3 parts, assume it's unslashed domain (http://www.pootpoot.net)
-        ## add a slash and re-split
-        if len(parts) == 3:
-            url = url + '/'
-            parts = str(url).split('/')
+        ## if we got served something that's not html, just return it
+        if not result.headers['content-type'].startswith('text/html'):
+            self.response.set_status(result.status_code)
+            self.response.out.write(result.content)
+            return
 
-        ## build a base url for the proxied page...
-        base_url = '/'.join(parts[:-1]) + '/'
+        ## We've got html, let's see what kind of damage we can do....
+        content = result.content.decode('utf-8')
 
-        if result.status_code == 200:
-            ## stuff a header into pages which don't already have one
-            if not re.compile('<head>', re.IGNORECASE).search(content):
-                content = re.compile('<html>', re.IGNORECASE).sub('<html><head></head>', content, 1)
+        ## stuff a header into pages which don't already have one
+        if not re.compile('<head>', re.IGNORECASE).search(content):
+            content = re.compile('<html>', re.IGNORECASE).sub('<html><head></head>', content, 1)
 
-            ## base adjustment
-            content = re.compile('<head>', re.IGNORECASE).sub('<head><base href="' + base_url + '"/>', content, 1)
+        ## base adjustment
+        base_url = urlparse.urlunparse((parts[0], parts[1], parts[2], '', '', ''))
+        content = re.compile('<head>', re.IGNORECASE).sub('<head><base href="' + base_url + '"/>', content, 1)
 
-            ## insert the script
-            content = re.compile('</head>', re.IGNORECASE).sub('''
+        ## insert the script
+        content = re.compile('</head>', re.IGNORECASE).sub('''
 <script type="text/javascript" src="%spootpoot.js"></script>
 <script type="text/javascript">
 function pootifier () {
@@ -75,21 +92,20 @@ function add_pootifier () {
 </script>
 </head>''' % (integration.INTEGRATIONS['APP_ROOT_URL'], base_url, integration.INTEGRATIONS['APP_ROOT_URL'] + 'pootify?url='), content, 1)
 
-            ## call the script
-            if re.compile('</body>', re.IGNORECASE).search(content):
-                content = re.compile('</body>', re.IGNORECASE).sub('''
+        ## call the script
+        if re.compile('</body>', re.IGNORECASE).search(content):
+            content = re.compile('</body>', re.IGNORECASE).sub('''
 <script type="text/javascript">
 add_pootifier();
 </script>
 </body>''', content, 1)
-                pass
-            else:
-                content = re.compile('</head>', re.IGNORECASE).sub('''
+            pass
+        else:
+            content = re.compile('</head>', re.IGNORECASE).sub('''
 <script type="text/javascript">
 add_pootifier();
 </script>
 </head>''', content, 1)
-
 
         self.response.set_status(result.status_code)
         self.response.out.write(content)
